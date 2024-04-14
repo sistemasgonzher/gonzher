98303e880531f3911670fb0563e9534f37fdb131
Emmanuel Hernandez <e805177@gmail.com>
2024-04-14 02:28:08 -0600

    fix: better layout was added, footer and new entry

---
+++ src/data/footerLinks.ts
@@ -0,0 +1,11 @@
+const footerLinks = [
+  {
+    title: "Social Links",
+    links: [
+      { title: "Instagram", href: "https://instagram.com/emmanuelh.dev" },
+      { title: "Github", href: "https://github.com/emmanuelh-dev" },
+    ],
+  },
+];
+
+export default footerLinks;
--- src/data/siteMetadata.ts
+++ src/data/siteMetadata.ts
@@ -1,6 +1,6 @@
 const siteMetadata = {
   title: "Tailwind Astro Blog Starter",
-  description: "A simple blog starter using Tailwind CSS and Astro",
+  description: "A simple blog starter using Tailwind CSS and Astro, with a focus on accessibility and performance for developers by developers.",
 };
 
 export default siteMetadata;
